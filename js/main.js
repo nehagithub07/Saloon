6fdcf26e34449f8436b2421698e087f145c31382
https://github.com/nehagithub07/ <nehasaniya465@gmail.com>
2025-02-19 14:11:05 +0530

    first commit

---
+++ js/main.js
@@ -0,0 +1,80 @@
+(function ($) {
+    "use strict";
+
+    // Spinner
+    var spinner = function () {
+        setTimeout(function () {
+            if ($('#spinner').length > 0) {
+                $('#spinner').removeClass('show');
+            }
+        }, 1);
+    };
+    spinner();
+    
+    // Initiate the wowjs
+    if (typeof WOW !== 'undefined') {
+        new WOW().init();
+    }
+
+    // Sticky Navbar
+    $(window).scroll(function () {
+        if ($(this).scrollTop() > 300) {
+            $('.sticky-top').addClass('shadow-sm').css('top', '0px');
+        } else {
+            $('.sticky-top').removeClass('shadow-sm').css('top', '-100px');
+        }
+    });
+
+    // Back to top button
+    $(window).scroll(function () {
+        if ($(this).scrollTop() > 300) {
+            $('.back-to-top').fadeIn('slow');
+        } else {
+            $('.back-to-top').fadeOut('slow');
+        }
+    });
+
+    $('.back-to-top').click(function () {
+        $('html, body').animate({ scrollTop: 0 }, 1500, 'easeInOutExpo');
+        return false;
+    });
+
+    // Testimonials carousel
+    if ($('.testimonial-carousel').length > 0) {
+        $('.testimonial-carousel').owlCarousel({
+            autoplay: true,
+            smartSpeed: 1000,
+            loop: true,
+            nav: false,
+            dots: true,
+            items: 1,
+            dotsData: true,
+        });
+    }
+
+    let slideIndex = 1;
+showSlides(slideIndex);
+
+window.plusSlides = function (n) {
+    showSlides(slideIndex += n);
+};
+
+window.currentSlide = function (n) {
+    showSlides(slideIndex = n);
+};
+
+function showSlides(n) {
+    let slides = document.getElementsByClassName("mySlides");
+    let dots = document.getElementsByClassName("dot");
+    if (n > slides.length) { slideIndex = 1 }
+    if (n < 1) { slideIndex = slides.length }
+    for (let i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";
+    }
+    for (let i = 0; i < dots.length; i++) {
+        dots[i].className = dots[i].className.replace(" active", "");
+    }
+    slides[slideIndex - 1].style.display = "block";
+    dots[slideIndex - 1].className += " active";
+}
+})(jQuery);
